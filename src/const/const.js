15249a9b5a163cdc78d988cc6e01bc68fce9a230
carlosgjim98 <carlosgjim.98@gmail.com>
2024-12-07 23:28:45 +0100

    first-commit

---
+++ src/const/const.js
@@ -0,0 +1,258 @@
+
+
+export const studies = [
+  {
+    title: "Grado Superior en Desarrollo de Aplicaciones Multiplataforma",
+    corporation: "iFP, Sevilla",
+    date: "sep 2022 - jun 2024",
+  },
+  {
+    title: "Grado en Publicidad y Relaciones públicas",
+    corporation: "Universidad EUSA, Sevilla",
+    date: "sep 2018 - sep 2022",
+  },
+  {
+    title: "E.S.O y Bachiller",
+    corporation: "El centro Inglés, El Puerto de Santa María, Cádiz",
+    date: "sep 2010 - jun 2017",
+  },
+];
+
+export const IconKeys = {
+  angular: "angular",
+  ionic: "ionic",
+  html: "html",
+  css: "css",
+  javaScript: "javaScript",
+  react: "react",
+  astro: "astro",
+  materialUi: "materialUi",
+  nextjs: "nextjs",
+  redux: "redux",
+  styled: "styled",
+  tailwind: "tailwind",
+  radix: "radix",
+  reactQuery: "reactQuery",
+  typeScript: "typeScript",
+  zustand: "zustand",
+  git: "git",
+  github: "github",
+  stripe: "stripe",
+  shadcn: "shadcn",
+  nodejs: "nodejs",
+  mongodb: "mongodb",
+  vite: "vite",
+  visualStudio: "visualStudio"
+  // here add more icon keys
+};
+
+export const skills = [
+  {
+    title: "Angular",
+    icon: IconKeys.angular
+  },
+  {
+    title: "Ionic",
+    icon: IconKeys.ionic,
+  },
+  {
+    title: "Html",
+    icon: IconKeys.html,
+  },
+  {
+    title: "Css",
+    icon: IconKeys.css,
+  },
+  {
+    title: "JavaScript",
+    icon: IconKeys.javaScript,
+  },
+  {
+    title: "Astro",
+    icon: IconKeys.astro,
+  },
+
+  {
+    title: "React Js",
+    icon: IconKeys.react,
+  },
+  {
+    title: "Next Js",
+    icon: IconKeys.nextjs,
+  },
+  {
+    title: "Git",
+    icon: IconKeys.git,
+  },
+
+  {
+    title: "Github",
+    icon: IconKeys.github,
+  },
+
+
+  {
+    title: "Node Js",
+    icon: IconKeys.nodejs,
+  },
+  {
+    title: "TypeScript",
+    icon: IconKeys.typeScript,
+  },
+
+  {
+    title: "Visual Studio",
+    icon: IconKeys.visualStudio,
+  },
+  
+ 
+];
+
+export const projects = [
+  {
+    img: "../../../img/little_thai.webp",
+    title: "Little Thai",
+    link: "https://github.com/carlosgjim98/app-little-thai",
+    description: `Aplicación para pedidos, reservas y promociones de los restaurantes Little Thai."En Desarrollo "`,
+    technologies: [
+      {
+        icon: IconKeys.typeScript,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.ionic
+      },
+      {
+        icon: IconKeys.angular
+      },
+    ],
+  },
+  {
+    img: "../../../img/guamigo.webp",
+    title: "Guaumigo",
+    link: "https://github.com/carlosgjim98/app-guaumigos",
+    description: `App que conecta dueños de perros con paseadores de confianza en toda España y no cobra comisión a paseadores.  `,
+    technologies: [
+      {
+        icon: IconKeys.typeScript,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.ionic
+      },
+      {
+        icon: IconKeys.angular
+      },
+    ],
+  },
+  {
+    img: "../../../img/policia.webp",
+    title: "App denuncias policia",
+    link: "https://github.com/carlosgjim98/app-policia",
+    description: `Una app de la Policía Local de Alcúdia para crear denuncias, reportar incidentes y acceder a servicios de seguridad de manera rápida y eficiente.`,
+    technologies: [
+      {
+        icon: IconKeys.typeScript,
+      },
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.ionic
+      },
+      {
+        icon: IconKeys.angular
+      },
+    ],
+  },
+  {
+    img: "../../../img/jdv.webp",
+    title: "JDV",
+    link: "https://store-games-site.netlify.app/",
+    description: `Una app intuitiva para invertir, gestionar tu cartera y seguir el mercado en tiempo real.`,
+    technologies: [
+      {
+        icon: IconKeys.typeScript,
+      },
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.ionic
+      },
+      {
+        icon: IconKeys.angular
+      },
+    ],
+  },
+  {
+    img: "../../../img/citas.webp",
+    title: "App de citas",
+    link: "https://github.com/carlosgjim98/app-citas",
+    description: `Una app de citas para conectar personas afines, facilitar encuentros y crear conexiones reales...`,
+    technologies: [
+      {
+        icon: IconKeys.typeScript,
+      },
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.ionic
+      },
+      {
+        icon: IconKeys.angular
+      },
+    ],
+  },
+  {
+    img: "../../../img/app-musica.webp",
+    title: "WoWeLike",
+    link: "https://github.com/carlosgjim98/app-clickcomun",
+    description: `Una app de música para crear y compartir playlists, chatear con amigos y descubrir nuevos sonidos juntos.`,
+    technologies: [
+      {
+        icon: IconKeys.typeScript,
+      },
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.ionic
+      },
+      {
+        icon: IconKeys.angular
+      },
+    ],
+  },
+
+ 
+];
